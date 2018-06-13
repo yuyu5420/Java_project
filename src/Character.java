e7e427612067223288fd19ac13747738a9d35a95
lovecrysong <a0923862606@gmail.com>
2018-06-13 12:00:05 +0800

    Add files via upload

--- src/Character.java
+++ src/Character.java
@@ -52,7 +52,7 @@ public abstract class Character implements GameStateDefault {
 				this.right_movable = false;
 		}
 		
-
+		// check obstacle
 		if(this.up_movable) {
 			if(!Game.go[(this.ideal_up[0]-400)/100][(this.ideal_up[1])/100])
 				this.up_movable = false;
@@ -105,6 +105,56 @@ public abstract class Character implements GameStateDefault {
 		}
 	}
 
-	
-
+	public void check(int state_now) {
+		
+		if(ideal_X==this.Xcoordinate && ideal_Y==this.Ycoordinate) {
+			game.getKeyManager().state_now = game.getKeyManager().state_next;
+			if(game.getKeyManager().state_now!=0) {
+				game.getKeyManager().state = game.getKeyManager().state_now;
+			}
+			return;
+		} else {
+			if(state_now == KeyEvent.VK_UP || state_now == KeyEvent.VK_DOWN) {
+				if(Math.abs((ideal_Y-this.Ycoordinate))==this.pace) {
+					if(state_now == KeyEvent.VK_UP) {
+						this.Ycoordinate -= this.pace;
+					} else {
+						this.Ycoordinate += this.pace;
+					}
+					this.pace_cnt++;
+					game.getKeyManager().state_now = game.getKeyManager().state_next;
+					if(game.getKeyManager().state_now!=0) {
+						game.getKeyManager().state = game.getKeyManager().state_now;
+					}
+				} else {
+					if(state_now == KeyEvent.VK_UP) {
+						this.Ycoordinate -= this.pace;
+					} else {
+						this.Ycoordinate += this.pace;
+					}
+					this.pace_cnt++;
+				}
+			} else {
+				if(Math.abs((ideal_X-this.Xcoordinate))==this.pace) {
+					if(state_now == KeyEvent.VK_LEFT) {
+						this.Xcoordinate -= this.pace;
+					} else {
+						this.Xcoordinate += this.pace;
+					}
+					this.pace_cnt++;
+					game.getKeyManager().state_now = game.getKeyManager().state_next;
+					if(game.getKeyManager().state_now!=0) {
+						game.getKeyManager().state = game.getKeyManager().state_now;
+					}
+				} else {
+					if(state_now == KeyEvent.VK_LEFT) {
+						this.Xcoordinate -= this.pace;
+					} else {
+						this.Xcoordinate += this.pace;
+					}
+					this.pace_cnt++;
+				}
+			}
+		}
+	}
 }
--- src/User.java
+++ src/User.java
@@ -57,61 +57,6 @@ public abstract class User extends Character {
 		} 
 		
 	}
-
-	private void check(int state_now) {
-		
-		if(ideal_X==this.Xcoordinate && ideal_Y==this.Ycoordinate) {
-			game.getKeyManager().state_now = game.getKeyManager().state_next;
-			if(game.getKeyManager().state_now!=0) {
-				game.getKeyManager().state = game.getKeyManager().state_now;
-			}
-			return;
-		} else {
-			if(state_now == KeyEvent.VK_UP || state_now == KeyEvent.VK_DOWN) {
-				if(Math.abs((ideal_Y-this.Ycoordinate))==this.pace) {
-					if(state_now == KeyEvent.VK_UP) {
-						this.Ycoordinate -= this.pace;
-					} else {
-						this.Ycoordinate += this.pace;
-					}
-					this.pace_cnt++;
-					game.getKeyManager().state_now = game.getKeyManager().state_next;
-					if(game.getKeyManager().state_now!=0) {
-						game.getKeyManager().state = game.getKeyManager().state_now;
-					}
-				} else {
-					if(state_now == KeyEvent.VK_UP) {
-						this.Ycoordinate -= this.pace;
-					} else {
-						this.Ycoordinate += this.pace;
-					}
-					this.pace_cnt++;
-				}
-			} else {
-				if(Math.abs((ideal_X-this.Xcoordinate))==this.pace) {
-					if(state_now == KeyEvent.VK_LEFT) {
-						this.Xcoordinate -= this.pace;
-					} else {
-						this.Xcoordinate += this.pace;
-					}
-					this.pace_cnt++;
-					game.getKeyManager().state_now = game.getKeyManager().state_next;
-					if(game.getKeyManager().state_now!=0) {
-						game.getKeyManager().state = game.getKeyManager().state_now;
-					}
-				} else {
-					if(state_now == KeyEvent.VK_LEFT) {
-						this.Xcoordinate -= this.pace;
-					} else {
-						this.Xcoordinate += this.pace;
-					}
-					this.pace_cnt++;
-				}
-			}
-		}
-		
-
-	}
 	
 	
 }
